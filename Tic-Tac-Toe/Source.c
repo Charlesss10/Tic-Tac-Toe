ade17a3b2f6a3a35e82972f39425ef6a3ed108a7
Eboson Charles Ikenna <ebosoncharles77@gmail.com>
2023-08-31 17:21:29 +0200

    3rd commit
    Player1 is now allowed to select between X and O to start with. Also player now have to possibility of choosing to play against the AI or another Human.

--- Tic-Tac-Toe/Source.c
+++ Tic-Tac-Toe/Source.c
@@ -11,19 +11,24 @@ typedef enum {
 
 //functions prototype
 void displayBoard(char* boardPtr, int* size);
-void insert(char* boardPtr, int* size, char* turn);
+void insert(char* boardPtr, int* size, char* turn, char* player1Turn);
 void switchTurn(char* turn);
 void checkWinStatus(char* boardPtr, int* size, int* winStatus, char* turn);
 void keyBoardBuffer();
 void checkDrawStatus(char* boardPtr, int* size, int* drawStatus);
+void pickTurn(char* turn, char* player1Turn, int *choice);
+void pickOpponent(int* choice);
 
 int main() {
 
 	//An empty board array
 	char board[] = { '-','-', '-', '-', '-', '-', '-', '-', '-', };
 
-	//Users turn defaultly set to X, meaning X always starts first
-	char turn = 'X';
+	//Users turn changes after every loop
+	char turn, player1Turn;
+
+	//choice to play against the AI or Human
+	int choice;
 
 	//pointer to the board
 	char *boardPtr;
@@ -49,32 +54,42 @@ int main() {
 	//funtion call to display the board
 	displayBoard(boardPtr, &size);
 
-	do {
+	//Function call to enable player choose between X or O
+	pickTurn(&turn, &player1Turn, &choice);
 
-		//function call to get players choice
-		insert(boardPtr, &size, &turn);
+	//choice 2 means the player 1 wants to play against a Human opponent
+	if (choice == 2) {
+		do {
 
-		//function call to check winning status
-		checkWinStatus(boardPtr, &size, &winStatus, &turn);
+			//function call to get players choice
+			insert(boardPtr, &size, &turn, &player1Turn);
 
-		//function call to check draw status
-		checkDrawStatus(boardPtr, &size, &drawStatus);
+			//function call to check winning status
+			checkWinStatus(boardPtr, &size, &winStatus, &turn);
 
-		//checking win or draw status
-		if (winStatus == 1) {
-			printf("\nCongratulations '%c'!!!, You just won the game.", turn);
-			programEnd = true;	
-		}
-		else if (drawStatus == 1) {
-			printf("\nThis game is a tie. Better luck next time!.");
-			programEnd = true;
-		}
-		else {
-			//function call for switching players turn
-			switchTurn(&turn);
-		}
+			//function call to check draw status
+			checkDrawStatus(boardPtr, &size, &drawStatus);
 
-	} while (programEnd == false);
+			//checking win or draw status
+			if (winStatus == 1) {
+				printf("\nCongratulations '%c'!!!, You just won the game.", turn);
+				programEnd = true;
+			}
+			else if (drawStatus == 1) {
+				printf("\nThis game is a tie. Better luck next time!.");
+				programEnd = true;
+			}
+			else {
+				//function call for switching players turn
+				switchTurn(&turn);
+			}
+
+		} while (programEnd == false);
+	}
+	//choice 1 means the player 1 wants to play against the AI
+	else if (choice == 1) {
+
+	}
 
 	getchar();
 
@@ -92,12 +107,21 @@ void displayBoard(char *boardPtr, int *size) {
 	}
 }
 
-void insert(char* boardPtr, int *size, char* turn) {
+void insert(char* boardPtr, int *size, char* turn, char* player1Turn) {
 	//index on the board
 	int index;
+	int playerNumber = 0;
+
+	//Confirming the player name to be displayed on the console
+	if (*turn == *player1Turn) {
+		playerNumber = 1;
+	}
+	else {
+		playerNumber = 2;
+	}
 
 	//Getting index from player
-	printf("%c turn, Enter your index on the board(1 - 9): ", *turn);
+	printf("Player %d (%c) turn, Enter your index on the board(1 - 9): ", playerNumber, *turn);
 
 	//verifying players chosen index
 	while (scanf("%d", &index) != 1 || index < 1 || index > 9 || boardPtr[index - 1] == 'X' || boardPtr[index - 1] == 'O') {
@@ -182,6 +206,57 @@ void checkDrawStatus(char* boardPtr, int* size, int* drawStatus) {
 			*drawStatus = 0;
 		}
 	}
+}
+
+void pickTurn(char* turn, char* player1Turn, int *choice) {
+
+	//Allowing player to choose between X or O
+	printf("Player 1, pick between X and O: ");
+	scanf("%c", &*turn);
+
+	//converting from small alphabet ro capital alphabet
+	if (*turn == 'x') {
+		*turn = 'X';
+	}
+	else if (*turn == 'o') {
+		*turn = 'O';
+	}
+
+	*player1Turn = *turn;
+
+	printf("Player 1 is %c\n", *turn);
+
+	//Function call to enable player1 choose opponent either AI or Human
+	pickOpponent(choice);
+
+	//Displaying either the AI or the player2
+	if (*choice == 2) {
+		if (*turn == 'X') {
+			printf("Player 2 is O\n");
+		}
+		else if (*turn == 'O') {
+			printf("Player 2 is X\n");
+		}
+	}
+	else if (*choice == 1) {
+		if (*turn == 'X') {
+			printf("AI is O\n");
+		}
+		else if (*turn == 'O') {
+			printf("AI is X\n");
+		}
+	}
+
+}
+
+void pickOpponent(int *choice) {
+	//Player1 given the chance to select opponent of choice
+	printf("Pick opponent \n(1. AI; 2. Human): ");
+	while (scanf("%d", &*choice) != 1 || *choice < 1 || *choice > 2) {
+		printf("Try again. (Must be 1 or 2): ");
+		keyBoardBuffer();
+	}
+	keyBoardBuffer();
 }
 
 void keyBoardBuffer() {
